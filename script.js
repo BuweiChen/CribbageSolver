d159598d3035dca2b66dc6d551425c54a9f13022
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-17 21:29:26 -0500

    Make card objects from hand

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const handElems = [...document.querySelectorAll('div[class^="card "][class$=" up bottom"]')];
-const hand = handElems.map(cardElem => cardElem.getAttribute("class").substr(5, 3).trim());
+const hand = handElems.map(cardElem => cardElem.getAttribute("class").substring(5, 8).trim());
 // e.g. hand = [s1, d2, h3, c10]
+
+const handCards = hand.map(card => {
+    let c = {
+        rank: Number(card.substring(1)), 
+        suit: card.substring(0, 1)
+    }
+    return c;
+});
